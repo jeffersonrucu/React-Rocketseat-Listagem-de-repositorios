d61b7317b5221a18fd96fc0878cd8773fc61aabb
jeffersonrucu <jefferoidtex7@gmail.com>
2021-11-24 23:04:23 -0300

    Listagem Repositorios

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/RepositoryList'
+import './styles/gloval.scss'
+
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  )
+}
